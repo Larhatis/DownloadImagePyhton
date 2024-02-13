d70839c7cb6def71583c5a606d52bf23ca7cb6a2
Larhatis <39920084+Larhatis@users.noreply.github.com>
2024-02-13 03:40:15 +0100

    ajoute du scroll afin de charger toute la page, mais firefox handless

--- README.md
+++ README.md
@@ -7,5 +7,10 @@ Ce script Python utilise Selenium pour accéder à une page web, extraire les UR
 
 Assurez-vous d'avoir Python installé sur votre machine. Vous pouvez installer les dépendances nécessaires en exécutant la commande suivante :
 
+Nécessite Firefox
+
 ```bash
-pip install selenium requests_toolbelt
+pip install selenium
+pip install webdriver_manager
+pip install requests-toolbelt
+
--- downloadimage.py
+++ downloadimage.py
@@ -1,10 +1,12 @@
-from selenium import webdriver
-from requests_toolbelt import sessions
 import os
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+from requests_toolbelt import sessions
 import uuid
 import shutil
+import time
 
-# Fonction pour télécharger une image
 def telecharger_image(url_image, nom_fichier):
     # Définir la session avec gestion du CORS
     session = sessions.BaseUrlSession(base_url=url_image)
@@ -12,7 +14,7 @@ def telecharger_image(url_image, nom_fichier):
     # Envoyer une requête GET pour obtenir l'image
     reponse = session.get(url_image)
 
-    # Vérifier le code de status
+    # Vérifier le code de statut
     if reponse.status_code == 200:
         # Enregistrer l'image dans un fichier
         with open(nom_fichier, "wb") as fichier:
@@ -29,22 +31,42 @@ os.makedirs(dossier_destination, exist_ok=True)
 # Demander à l'utilisateur l'URL de la page web
 url_page = input("Entrez l'URL de la page web : ")
 
-# Démarrer le driver Selenium
-driver = webdriver.Chrome()
+# Démarrer le driver Selenium en mode Headless avec le gestionnaire de pilotes
+options = Options()
+options.headless = True  # Firefox sans interface graphique mais ça marche pas, il s'ouvre quand même
 
-# Accéder à la page web
-driver.get(url_page)
+# Utiliser le GeckoDriverManager pour récupérer le chemin de l'exécutable
+gecko_path = GeckoDriverManager().install()
 
-# Trouver toutes les balises img
-images = driver.find_elements("css selector", "img")
+# Créer une instance du pilote Firefox avec le mode headless
+driver = webdriver.Firefox(options=options)
 
-# Télécharger les images
-for image in images:
-    # Générer un nom de fichier unique avec un UUID
-    nom_fichier = os.path.join(dossier_destination, f"image_{str(uuid.uuid4())}.jpg")
+try:
+    driver.get(url_page)
 
-    # Télécharger l'image
-    telecharger_image(image.get_attribute('src'), nom_fichier)
+    # Défilement de la page jusqu'en bas 
+    scroll_height = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
 
-# Fermer le driver Selenium
-driver.quit()
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)  # Attendre le déliement de la page
+        new_scroll_height = driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);")
+        if new_scroll_height == scroll_height:
+            break
+        scroll_height = new_scroll_height
+
+    # Attendre de défilement de la page
+    driver.implicitly_wait(5)
+
+    # Rechercher toutes les balises img sur la page
+    images = driver.find_elements("css selector", "img")
+
+    # DL des images
+    for image in images:
+        # Générer un nom de fichier unique pour chaque image
+        nom_fichier = os.path.join(dossier_destination, f"image_{str(uuid.uuid4())}.jpg")
+
+        telecharger_image(image.get_attribute("src"), nom_fichier)
+finally:
+    # Fermeture du navigateur
+    driver.quit()
